24b8cdb8f0f9a930cfdbe1387baaa8be89fa743c
Jayakumar L <ljayakumar05@gmail.com>
2021-09-03 17:41:30 +0530

    javascript-code

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,36 @@
-document.getElementById('demo').innerHTML= "hello world JAI kumar";
+// document.getElementById('demo').innerHTML= "hello world JAI kumar";
 
-function test(){
-    var a = "3";
-    var b = "8";
-
-
-   [a, b] = [b, a];
+// function test(){
+//     var a = "3";
+//     var b = "8";
 
 
+//    [a, b] = [b, a];
 
-    console.log("a is" + a);
-    console.log("a is" + b);
+
+
+//     console.log("a is" + a);
+//     console.log("a is" + b);
+// }
+
+// test();
+
+
+// document.querySelector("nav-link").addEventListener("click", clikbtn());
+
+// function clikbtn(){
+//     // console.log("btnclike");
+//    alert("Hello JAI");
+// }
+
+// document.getElementById("btnclik").addEventListener("click", clikbtn());
+ 
+// function clikbtn(){
+//     alert("Click ok");
+// }
+
+document.getElementById("btncliked").addEventListener("click", handleClick);
+
+function handleClick(){
+   console.log("Welcome JAI");
 }
-
-test();
